38308ae7e13522a585400fbc96e9f944b4f8c6bc
Elshaman <cbuitragoo.2023@gmail.com>
2025-01-22 18:23:56 -0500

    personalized_buttons

--- App.tsx
+++ App.tsx
@@ -1,24 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FAB from './componentes/FAB';
 
 export default function App() {
-
-  const [ count, setCount ] = useState(10)
+  const [count, setCount] = useState(10);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.textHuge}>{ count }</Text>
-        <Pressable
-          style={styles.floatingButton}
-          onPress={()=> setCount(count + 1) }
-          onLongPress={()=>setCount(0)}
-        >
-          <Text style={{ color: 'white'}}>
-            +1
-          </Text>
-        </Pressable>
-      <StatusBar style="dark"/>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      {/* <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}>+1</Text>
+      </Pressable> */}
+
+      <FAB
+        label="+1"
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+        position="right"
+      />
+
+      <FAB label="Reset" onPress={() => setCount(0)} position="left" />
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -30,24 +39,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textHuge:{
-      fontSize: 120,
-      fontWeight: '100'
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100',
   },
-  floatingButton: {
-    position: 'absolute',
-    bottom: 50, 
-    right: 50, 
-    backgroundColor: '#65658f', 
-    padding: 20, 
-    borderRadius:15,
-    shadowColor: '#000', 
-    shadowOffset:{
-      width: 0,
-      height: 4
-    }, 
-    shadowOpacity: 0.3, 
-    elevation: 3,
-    shadowRadius: 4
-  }
 });
